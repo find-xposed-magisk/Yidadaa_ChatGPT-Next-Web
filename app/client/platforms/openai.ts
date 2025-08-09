38ac502d8008002735166ecbe7fffa150bbe922b
Sam <seudxg@yeah.net>
2025-08-09 16:57:31 +0800

    Add support for GPT5

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -231,7 +231,7 @@ export class ChatGPTApi implements LLMApi {
         messages,
         stream: options.config.stream,
         model: modelConfig.model,
-        temperature: !isO1OrO3 ? modelConfig.temperature : 1,
+        temperature: (!isO1OrO3 && !isGpt5) ? modelConfig.temperature : 1,
         presence_penalty: !isO1OrO3 ? modelConfig.presence_penalty : 0,
         frequency_penalty: !isO1OrO3 ? modelConfig.frequency_penalty : 0,
         top_p: !isO1OrO3 ? modelConfig.top_p : 1,
